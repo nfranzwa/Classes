8b1d38d89cf2bb3c62e63a3faf8addc898503325
Nicholas Franzwa <nfranzwa@ucsd.edu>
2023-08-26 22:29:54 -0700

    hw3

--- MAE40/hw3.m
+++ MAE40/hw3.m
@@ -1,17 +1,17 @@
 clear,clc, close all, syms s Vi Vs C L R_load Vo R c_1 
 
 % x={I_L,   I_C,     I_R,     I_Rload,  Vo,  Vm}  <-- unknown vector
-A  =[L*s    0       0       0       0    1  ;   % I_a*R_a + V1 = Vo
-      0   1/(C*s)   0       0       1   -1  ;   % I_b/(C_b*s) + V2 = Vo
-      0     0       R       0      -1    0  ;   % I_d*R_d - V2 = 0
-      0     0       0     R/c_1   -1    0  ;   % I_c/(C_c*s) - V1 + V2 = 0
-      1    -1       0       0       0    0  ;   % I_b + I_c - I_d = 0
-      0    -1       1       1       0    0  ];   % I_a - I_c = 0
+A  =[L*s    0       0       0       0    1  ;   % L*s*I_L + Vm = Vi
+      0   1/(C*s)   0       0       1   -1  ;   % I_C/(C*s) + Vo - Vm = 0
+      0     0       R       0      -1    0  ;   % I_R*R - Vo = 0
+      0     0       0     R/c_1    -1    0  ;   % I_Rload*R_load - Vo = 0 
+      1    -1       0       0       0    0  ;   % I_L-I_C = 0
+      0    -1       1       1       0    0  ];  % I_R + I_Rload - I_C = 0
    
 b  =[Vi; 0; 0; 0; 0; 0];
 x=A\b; Vo_notch_huh=simplify(x(5))
 latex(Vo_notch_huh);
-omega4=10; Q=5; F_notch_huh=RR_tf([omega4 0],[1 (1/Q)*omega4 omega4^2]);
+omega4=10; Q=5; F_notch_huh=RR_tf([(1/Q)*omega4 0],[1 (1/Q)*omega4 omega4^2]);
 figure(1)
 RR_bode(F_notch_huh)  
 title('Bode Plot')
